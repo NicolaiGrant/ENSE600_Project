717c9e28b1a0201cf27d96f35eb2ebc32df9f234
RYZEN RTX <RYZEN RTX@NicolaiGrant>
2023-09-24 17:49:31 +1300

    Added bot class to extend player class for computer game mode

---
+++ ENSE600_Project2/src/game/Bot.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package game;
+
+import GUI.Colours;
+import java.awt.Color;
+import java.util.Random;
+
+public class Bot extends Player
+{
+    Random random;
+    String name;
+    
+    public Bot(Color colour)
+    {
+        super("Bot", colour);
+        this.random = new Random();
+        this.name = "Bot";
+    }
+        
+    @Override
+    public String getName()
+    {
+        return this.name;
+    }
+
+    public void move(Board board)
+    {
+        // Places the piece in a random column
+        
+        int x = random.nextInt(7) + 1;
+        if(!board.isRowFull(x))
+        {
+            board.dropPiece(x, disc);
+            //this.currentPlayer++;
+        }
+    }
+}
